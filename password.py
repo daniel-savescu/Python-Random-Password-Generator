8382c7e475413dac5f8090223a39a66b3664c8a7
Daniel Savescu <savescu.daniel.cristian@gmail.com>
2023-05-15 16:31:43 +0300

    initial commit

---
+++ password.py
@@ -0,0 +1,23 @@
+import random
+import string
+import secrets
+
+def generate_password(passwordSize):
+    password = ''
+    if passwordSize < 4:
+        print("Password must be 4 charachers long in size")
+    elif passwordSize >=4 and passwordSize <= 45:
+        lowerCaseLetters = string.ascii_lowercase
+        upperCaseLetters = string.ascii_uppercase
+        randomLetters = string.ascii_letters
+        digits = string.digits
+        punctuations = string.punctuation
+        alphabet = lowerCaseLetters + upperCaseLetters + randomLetters + digits + punctuations
+        password += ''.join(secrets.choice(alphabet) for i in range(passwordSize))
+        print(password)
+    elif passwordSize > 45:
+        print("Password size to large")
+#Prompt
+
+sizeOfPassword = int(input("Enter password length (4-45)"))
+generate_password(sizeOfPassword)
