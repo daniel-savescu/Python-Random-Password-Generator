37963a461f0e7e7811a1cf0d02d4916ac9b1be6e
Daniel Savescu <savescu.daniel.cristian@gmail.com>
2023-05-15 16:41:26 +0300

    save password to txt file

--- password.py
+++ password.py
@@ -7,17 +7,35 @@ def generate_password(passwordSize):
     if passwordSize < 4:
         print("Password must be 4 charachers long in size")
     elif passwordSize >=4 and passwordSize <= 45:
+        fileObject = open("password.txt", "w")
+        
         lowerCaseLetters = string.ascii_lowercase
+        
         upperCaseLetters = string.ascii_uppercase
+        
         randomLetters = string.ascii_letters
+        
         digits = string.digits
+        
         punctuations = string.punctuation
+        
         alphabet = lowerCaseLetters + upperCaseLetters + randomLetters + digits + punctuations
+        
         password += ''.join(secrets.choice(alphabet) for i in range(passwordSize))
+        
+        fileObject.write("Your password is : " + password)
+        
+        fileObject.close()
+        
         print(password)
+        
+        print("Check current folder of the script for the saved password")
+        
     elif passwordSize > 45:
         print("Password size to large")
 #Prompt
+
+
 
 sizeOfPassword = int(input("Enter password length (4-45)"))
 generate_password(sizeOfPassword)
